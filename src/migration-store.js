c17f2cea9be502a6f37d4de73b4241bc9612aebd
Adriean Khisbe <adriean.khisbe@live.fr>
2018-07-12 15:01:40 +0200

    Create a simple store that just take two stores as parameters

--- src/migration-store.js
+++ src/migration-store.js
@@ -1,1 +1,14 @@
-console.log('migrate session store'); // eslint-disable-line no-console
+module.exports = function(session) {
+  const Store = session.Store;
+  function MigrationStore(options) {
+    // §TODO ensure from and to store are present
+    this.fromStore = options.from;
+    this.toStore = options.to;
+    Store.call(this, options);
+  }
+
+  // Inherit from `Store`
+  MigrationStore.prototype.__proto__ = Store.prototype;
+
+  return MigrationStore;
+};
--- test/migration-store.test.js
+++ test/migration-store.test.js
@@ -1,1 +1,13 @@
-console.log('test store'); // eslint-disable-line no-console
+const test = require('ava');
+const session = require('express-session');
+const MigrationStore = require('../src/migration-store')(session);
+
+test('should be created and substore accessible', t => {
+  const store = new MigrationStore({
+    from: 12,
+    to: 42
+  });
+  t.true(store instanceof MigrationStore);
+  t.is(12, store.fromStore);
+  t.is(42, store.toStore);
+});
