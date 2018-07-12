a10d2c08f63907b626201088d49dbd3e826affb0
Adriean Khisbe <adriean.khisbe@live.fr>
2018-07-12 15:09:32 +0200

    Add dependencies to test against two backend

--- test/migration-store.test.js
+++ test/migration-store.test.js
@@ -1,13 +1,25 @@
 const test = require('ava');
+const AWS = require('aws-sdk')
 const session = require('express-session');
+const DynamoDbStore = require('connect-dynamodb')({session})
+const RedisStore = require('connect-redis')(session)
 const MigrationStore = require('../src/migration-store')(session);
+
+const makeDynamoDbStore = () => {
+  const client = new AWS.DynamoDB({
+    endpoint: new AWS.Endpoint('http://localhost:8000'),
+    region: 'eu-west-4'
+  });
+  return new DynamoDbStore({client, table: 'migration-session-test'});
+};
+const makeRedisStore = () => new RedisStore();
 
 test('should be created and substore accessible', t => {
   const store = new MigrationStore({
-    from: 12,
-    to: 42
+    from: makeRedisStore(),
+    to: makeDynamoDbStore()
   });
   t.true(store instanceof MigrationStore);
-  t.is(12, store.fromStore);
-  t.is(42, store.toStore);
+  t.true(store.fromStore instanceof RedisStore);
+  t.true(store.toStore instanceof DynamoDbStore);
 });
