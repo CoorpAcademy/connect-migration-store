4bf03897dc0d279a4a6ae8563c9b36fbe406808c
Adriean Khisbe <adriean.khisbe@live.fr>
2019-10-23 09:33:19 +0200

    Drop bluebird for vanilla fromCallback implementation :bird:

--- test/migration-store.test.js
+++ test/migration-store.test.js
@@ -1,8 +1,10 @@
 const test = require('ava');
-const Bromise = require('bluebird');
 const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
 const MigrationStore = require('../src/migration-store')(session);
+
+const fromCallback = fun =>
+  new Promise((resolve, reject) => fun((err, data) => (err ? reject(err) : resolve(data))));
 
 test.beforeEach(t => {
   t.context.from = new MemoryStore();
@@ -37,51 +39,49 @@ test('should not be created with invalid arguments', t => {
 
 test('should get session from "to" if defined', async t => {
   const store = new MigrationStore({from: t.context.from, to: t.context.to});
-  await Bromise.fromCallback(cb =>
+  await fromCallback(cb =>
     t.context.to.set('42', {cookie: {maxAge: 2000}, name: 'dynamodbStore'}, cb)
   );
-  const sess = await Bromise.fromCallback(cb => store.get('42', cb));
+  const sess = await fromCallback(cb => store.get('42', cb));
   t.deepEqual(sess, {cookie: {maxAge: 2000}, name: 'dynamodbStore'});
 });
 
 test('should get session from "from" if not defined in "to"', async t => {
   const store = new MigrationStore({from: t.context.from, to: t.context.to});
-  await Bromise.fromCallback(cb =>
+  await fromCallback(cb =>
     t.context.from.set('42', {cookie: {maxAge: 2000}, name: 'redisStore'}, cb)
   );
-  const sess = await Bromise.fromCallback(cb => store.get('42', cb));
+  const sess = await fromCallback(cb => store.get('42', cb));
   t.deepEqual(sess, {cookie: {maxAge: 2000}, name: 'redisStore'});
 });
 
 test('should get session from "from" if not defined in "to" and set back session in "to"', async t => {
   const store = new MigrationStore({from: t.context.from, to: t.context.to});
-  await Bromise.fromCallback(cb =>
+  await fromCallback(cb =>
     t.context.from.set('42', {cookie: {maxAge: 2000}, name: 'redisStore'}, cb)
   );
-  const sess = await Bromise.fromCallback(cb => store.get('42', cb));
+  const sess = await fromCallback(cb => store.get('42', cb));
   t.deepEqual(sess, {cookie: {maxAge: 2000}, name: 'redisStore'});
-  const newSess = await Bromise.fromCallback(cb => store.toStore.get('42', cb));
+  const newSess = await fromCallback(cb => store.toStore.get('42', cb));
   t.deepEqual(newSess, {cookie: {maxAge: 2000}, name: 'redisStore'});
 });
 
 test('should set session in "to"', async t => {
   const store = new MigrationStore({from: t.context.from, to: t.context.to});
-  await Bromise.fromCallback(cb =>
-    store.set('42', {cookie: {maxAge: 2000}, name: 'migration'}, cb)
-  );
-  const sess = await Bromise.fromCallback(cb => store.toStore.get('42', cb));
+  await fromCallback(cb => store.set('42', {cookie: {maxAge: 2000}, name: 'migration'}, cb));
+  const sess = await fromCallback(cb => store.toStore.get('42', cb));
   t.deepEqual(sess, {cookie: {maxAge: 2000}, name: 'migration'});
 });
 
 test('should destroy session in "to" and "from', async t => {
   const store = new MigrationStore({from: t.context.from, to: t.context.to});
-  await Bromise.fromCallback(cb =>
+  await fromCallback(cb =>
     store.fromStore.set('42', {cookie: {maxAge: 2000}, name: 'tobedestroyed'}, cb)
   );
-  await Bromise.fromCallback(cb =>
+  await fromCallback(cb =>
     store.toStore.set('42', {cookie: {maxAge: 2002}, name: 'tobedestroyed'}, cb)
   );
-  await Bromise.fromCallback(cb => store.destroy('42', cb));
-  const sess = await Bromise.fromCallback(cb => store.toStore.get('42', cb));
+  await fromCallback(cb => store.destroy('42', cb));
+  const sess = await fromCallback(cb => store.toStore.get('42', cb));
   t.is(sess, undefined);
 });
